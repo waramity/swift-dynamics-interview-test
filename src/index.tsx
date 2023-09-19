af9e0cf49e3c4af3bc56d0fdcd0ce74799037287
unknown <waramity.re@gmail.com>
2023-09-20 05:23:17 +0700

    redux toolkit setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './components/App/App'
+import { store } from './store'
+import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
-
+  </Provider>,
+  document.getElementById('root')
+)
